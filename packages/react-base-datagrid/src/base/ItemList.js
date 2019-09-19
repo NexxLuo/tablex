eccd768fc572238532e361be7115203a2c6e7aa7
NexxLuo <670435363@qq.com>
2019-09-19 23:07:46 +0800

    非虚拟滚动下，处理scrollToItem方法

--- packages/react-base-datagrid/src/base/ItemList.js
+++ packages/react-base-datagrid/src/base/ItemList.js
@@ -1,7 +1,9 @@
 import React from "react";
 
+
 class ItemList extends React.Component {
   outterElement = null;
+
   outerRef = ins => {
     let { outerRef } = this.props;
     if (typeof outerRef === "function") {
@@ -10,7 +12,7 @@ class ItemList extends React.Component {
     this.outterElement = ins;
   };
 
-  resetAfterIndex() {}
+  resetAfterIndex() { }
 
   scrollUpdateWasRequested = false;
   scrollTo = scrollOffset => {
@@ -20,7 +22,77 @@ class ItemList extends React.Component {
     }
   };
 
-  scrollToItem() {}
+  scrollToItem = (index, align = 'auto') => {
+
+    let el = this.outterElement;
+
+    if (el) {
+      let items = el.getElementsByClassName("tablex-table-row");
+
+      let itemCount = items.length;
+
+      index = Math.max(0, Math.min(index, itemCount - 1));
+
+      let item = items[index];
+      if (!item) {
+        return;
+      }
+
+      let scrollOffset = el.scrollTop;
+      let size = this.props.height;
+      let itemSize = item.offsetHeight;
+      let itemOffset = item.offsetTop;
+
+      const maxOffset = itemOffset;
+
+
+      const minOffset = Math.max(
+        0,
+        itemOffset - size + itemSize
+      );
+
+      if (align === 'smart') {
+        if (
+          scrollOffset >= minOffset - size &&
+          scrollOffset <= maxOffset + size
+        ) {
+          align = 'auto';
+        } else {
+          align = 'center';
+        }
+      }
+
+      let toOffset = el.scrollTop;
+
+      switch (align) {
+        case 'start':
+          toOffset = maxOffset;
+          break;
+        case 'end':
+          toOffset = minOffset;
+          break;
+        case 'center':
+          toOffset = Math.round(minOffset + (maxOffset - minOffset) / 2);
+          break;
+        case 'auto':
+        default:
+          if (scrollOffset >= minOffset && scrollOffset <= maxOffset) {
+            toOffset = scrollOffset;
+          } else if (scrollOffset < minOffset) {
+            toOffset = minOffset;
+          } else {
+            toOffset = maxOffset;
+          }
+      }
+
+
+      this.scrollTo(toOffset)
+    }
+
+
+
+
+  }
 
   onScroll = e => {
     let { onScroll } = this.props;
@@ -30,6 +102,7 @@ class ItemList extends React.Component {
         scrollOffset: e.target.scrollTop,
         scrollUpdateWasRequested: this.scrollUpdateWasRequested
       });
+      this.scrollUpdateWasRequested = false;
     }
   };
 
@@ -66,7 +139,6 @@ class ItemList extends React.Component {
           position: "relative",
           height: height,
           overflow: "auto",
-          willChange: "transform",
           direction: "ltr",
           ...style
         }}
@@ -74,7 +146,7 @@ class ItemList extends React.Component {
         onScroll={this.onScroll}
       >
         {innerElementType({
-          style: { height },
+          style: {},
           children: itemsArr
         })}
       </div>
