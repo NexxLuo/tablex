0db5b052db1841297354daec2a605d47aaab6f4c
NexxLuo <670435363@qq.com>
2019-10-27 20:51:20 +0800

    a

--- packages/site/src/index.js
+++ packages/site/src/index.js
@@ -1,16 +1,27 @@
-import React, { Component } from "react";
-import 'babel-polyfill';
+function autoNext() {
+  setInterval(function() {
+    let mainDocument = document.getElementsByTagName("iframe")[1]
+      .contentDocument;
 
-class index extends Component {
-  a = () => {
-    return new Promise(resolve => {
-      resolve();
-    });
-  };
+    let frames = mainDocument.getElementsByTagName("frameset")[1].children;
+    let leftFrame = frames[0].contentDocument;
+    let rightFrame = frames[1].contentDocument;
 
-  render() {
-    return <div />;
-  }
+    let remarkText = rightFrame.getElementById("tdRemark").innerText;
+    let nextButton = leftFrame.getElementById("btnNext");
+
+    let bl = remarkText.indexOf("已经学习完毕") > -1;
+
+    //console.log("auto next:", bl, remarkText);
+
+    if (bl === true) {
+      console.log("auto next waiting...");
+
+      nextButton.click();
+
+      console.log("auto next done...");
+    }
+  }, 5000);
 }
 
-export default index;
+
