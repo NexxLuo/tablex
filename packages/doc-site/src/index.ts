50ba9cf787714909e6f7e1eb75277b0e3b4223c0
nexxluo <670435363@qq.com>
2023-05-06 16:45:04 +0800

    删除老文档

--- packages/doc-site/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Basic } from './Basic';
-export { default as Editor } from './Editor';
-export { default as ColumnSummary } from './ColumnSummary';
-export { default as AutoRowSpan } from './AutoRowSpan';
