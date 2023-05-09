758bdcf4b76b0d1b596dd72fd011b8f6d61e95d3
nexxluo <670435363@qq.com>
2023-05-09 16:25:13 +0800

    完善文档部署

--- packages/doc/.dumirc.ts
+++ packages/doc/.dumirc.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'dumi';
 
+const BasePath = process.env.DOC_BASE || '/';
+
 export default defineConfig({
   outputPath: 'docs-dist',
+  publicPath: BasePath,
   favicons: false,
   themeConfig: {
     logo: '/logo-128.png',
